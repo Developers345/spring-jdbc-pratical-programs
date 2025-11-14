e95e06712e6cbc034f0ea203574b86f9e3456561
gireesh978 <polurugireesh169@gmail.com>
2025-11-14 18:49:02 +0530

    Added the multiple rows and dml operations logic in JdbcTemplate.

--- src/main/java/com/spring/jdbc/configuration/BikeDAO.java
+++ src/main/java/com/spring/jdbc/configuration/BikeDAO.java
@@ -1,10 +1,18 @@
 package com.spring.jdbc.configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class BikeDAO {
 
     private final String GET_BIKE_COUNT = "select count(1) from bike";
     private final String GET_BIKE_NAME = "select bike_name from bike where bike_no= ?";
     private final String GET_BIKE_BY_BIKE_NO = "select bike_no,bike_name,bike_color,bike_model from bike where bike_no= ?";
+    private final String GET_ALL_BIKES = "select bike_no,bike_name,bike_color,bike_model from bike";
+    private final String INSERT_BIKE_DATA = "insert into bike values(?,?,?,?)";
 
 
     private JdbcTemplate jdbcTemplate;
@@ -24,7 +32,36 @@ public class BikeDAO {
 
     public BikeBo getBikeInfo(int bikeNo)
     {
-        return jdbcTemplate.queryForObject(GET_BIKE_BY_BIKE_NO,(rs,rowNum)->{
+        return jdbcTemplate.queryForObject(GET_BIKE_BY_BIKE_NO,rowMapper,bikeNo);
+    }
+
+    public List<BikeBo> getAllBikes()
+    {
+          return jdbcTemplate.query(GET_ALL_BIKES,rowMapper);
+    }
+
+    public int addBike(BikeBo bikeBo)
+    {
+        return jdbcTemplate.update(INSERT_BIKE_DATA, bikeBo.getBikeNo(),bikeBo.getBikeName(), bikeBo.getBikeColor(), bikeBo.getBikeModel());
+    }
+
+    /* Lambda expressions are suitable for use the method only once
+    if multiple places we can use then Lambda expression not recommanded.
+
+    (rs,rowNum)->{
+        BikeBo bikeBo = null;
+        bikeBo = new BikeBo();
+        bikeBo.setBikeNo(rs.getInt(1));
+        bikeBo.setBikeName(rs.getString(2));
+        bikeBo.setBikeColor(rs.getString(3));
+        bikeBo.setBikeModel(rs.getString(4));
+        return bikeBo;
+    }
+    */
+    //We can go for anonymous inner class or private inner class
+    RowMapper<BikeBo> rowMapper = new RowMapper<BikeBo>() {
+        @Override
+        public BikeBo mapRow(ResultSet rs, int rowNum) throws SQLException {
             BikeBo bikeBo = null;
             bikeBo = new BikeBo();
             bikeBo.setBikeNo(rs.getInt(1));
@@ -32,8 +69,8 @@ public class BikeDAO {
             bikeBo.setBikeColor(rs.getString(3));
             bikeBo.setBikeModel(rs.getString(4));
             return bikeBo;
-        },bikeNo);
-    }
+        }
+    };
 
 
 }
--- src/main/java/com/spring/jdbc/configuration/JDBCConfigTest.java
+++ src/main/java/com/spring/jdbc/configuration/JDBCConfigTest.java
@@ -3,6 +3,9 @@ package com.spring.jdbc.configuration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class JDBCConfigTest {
 
     public static void main(String[] args) {
@@ -18,6 +21,21 @@ public class JDBCConfigTest {
 
         BikeBo bikeBo = bikeDAO.getBikeInfo(3);
         System.out.println(bikeBo);
+
+        BikeBo bikeBoObject = new BikeBo();
+        bikeBoObject.setBikeNo(4);
+        bikeBoObject.setBikeName("Pluser");
+        bikeBoObject.setBikeColor("Green");
+        bikeBoObject.setBikeModel("B4");
+
+        int status = bikeDAO.addBike(bikeBoObject);
+
+        System.out.println("row updated: "+" "+status);
+
+        List<BikeBo> bikeBos = bikeDAO.getAllBikes();
+        Stream sBikes = Stream.of(bikeBos);
+        //bikeBos.stream().forEach(System.out::println);
+        sBikes.forEach(System.out::println);
 
     }
 }
